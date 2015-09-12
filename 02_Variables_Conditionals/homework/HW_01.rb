65ce0b07a4d87a089cac0912bc039be12aae3745
Sam Backus <Samuel.Backus@gmail.com>
2015-09-12 17:22:03 -0400

    the homework is done

--- 02_Variables_Conditionals/homework/HW_01.rb
+++ 02_Variables_Conditionals/homework/HW_01.rb
@@ -10,18 +10,18 @@
 #
 ###############################################################################
 #
-# We're ready to program! To practice our Ruby skills lets create a secret number game. 
-# In this game players have three tries to guess a secret number between 1 and 10. 
+# We're ready to program! To practice our Ruby skills lets create a secret number game.
+# In this game players have three tries to guess a secret number between 1 and 10.
 #
 # Here are some guidelines to building your game:
 #
 #	Intros
-# 	-	Welcome the player to your game. Let them know who created the game. 
+# 	-	Welcome the player to your game. Let them know who created the game.
 #	  -	Ask for the player's name then personally greet them by printing to the screen, "Hi player_name!"
-#	  -	Any good game will communicate the rules. Let the player know they must guess a number between 1 and 10 
+#	  -	Any good game will communicate the rules. Let the player know they must guess a number between 1 and 10
 #		and that they only have 3 tries to do so.
 #
-#	Functionality: 
+#	Functionality:
 #	 -	Hard code the secret number. Make it a random number between 1 and 10.
 #	 - 	Ask the user for their guess.
 #	 -	Verify if they were correct. If the player guesses correctly they win the game they should be congratulated and the game should end.
@@ -31,8 +31,9 @@
 #	 -	If they don't guess correctly after 3 tries, print that the Player lost and the game is over. Also let them know what the `secret_number` was.
 #
 # Make sure to add helpful comments to your code to document what each section does.
-# 
+#
 # Remember to cast input from the Player into the appropriate data type.
 #
 ###############################################################################
 
+#I did it!
